02b9d6d55e6c7f25f847da91156d0e9463658a8f
Nanrou <kkkcomkkk@qq.com>
2016-11-28 00:09:17 +0800

    updata

--- test7_blog/app/forms.py
+++ test7_blog/app/forms.py
@@ -2,8 +2,7 @@ from flask_wtf import Form
 from wtforms import TextField,BooleanField,PasswordField
 from wtforms.validators import Required
 
-class LoginFrom(Form):
-    name = TextField('Name',validators=[Required()])
-    password = PasswordField('password',validators=[Required()])
-    remember_me = BooleanField('Remember_me',defluat = False)
-    
+class LoginForm(Form):
+    name = TextField('Name', validators=[Required()])
+    password = PasswordField('password', validators=[Required()])
+    remember_me = BooleanField('Remember_me', default=False)
